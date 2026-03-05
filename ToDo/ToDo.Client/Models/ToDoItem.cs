69db3989dee96db9497ccc55d97c359474b2049b
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-03-05 15:23:12 +0100

    Todo Items from List

---
+++ ToDo/ToDo.Client/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.Client.Models;
+
+public class ToDoItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required string Text { get; set; } 
+    public DateTime Created { get; set; } = DateTime.Now;
+    public bool Completed { get; set; } = false;
+
+}
