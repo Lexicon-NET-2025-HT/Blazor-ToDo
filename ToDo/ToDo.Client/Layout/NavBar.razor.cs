3e6970c456bf5f5e2b0bf01eab5c01b4bc327235
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-03-05 14:35:04 +0100

    Add NavBar, Remove defaults (some)

---
+++ ToDo/ToDo.Client/Layout/NavBar.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ToDo.Client.Layout;
+
+public partial class NavBar
+{
+    [Parameter]
+    public string NavTitle { get; set; } = "Hej";
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
