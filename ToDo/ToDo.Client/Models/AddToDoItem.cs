34edcfd303182e40298f69600f1085b07913b343
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-03-06 11:19:55 +0100

    Validationworks

---
+++ ToDo/ToDo.Client/Models/AddToDoItem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Client.Models;
+
+public class AddToDoItem
+{
+    [Required(ErrorMessage = "Please enter a todo.")]
+    [MinLength(2, ErrorMessage = "Todo must be at least 2 characters.")]
+    [MaxLength(15, ErrorMessage = "Todo can’t be longer than 15 characters.")]
+    public string Text { get; set; } = string.Empty;
+}
